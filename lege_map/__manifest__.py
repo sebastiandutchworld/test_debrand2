e2dcf7cf735730f8c9ffe9200a0a95d457ed3d41
sebastiandutchworld <99655310+sebastiandutchworld@users.noreply.github.com>
2022-08-11 16:07:40 +0200

    Add files via upload

---
+++ lege_map/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Lege map",
+    "summary": """Remove Odoo branding in sent emails and the buttons in the emails
+    """,
+    "version": "14.0.1.0.0",
+    "website": "dutchworld.nl",
+    "author": """Dutchworld Solutions""",
+    "license": "AGPL-3",
+    'data': [
+        'views/views.xml',
+    ],
+    "installable": True,
+    "depends": ["mail","sale"],
+}
